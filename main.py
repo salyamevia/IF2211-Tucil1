8f0315b7080ee7b25edcbc3926132e3a25e72479
S Alya Mevia <alyameviia@gmail.com>
2021-01-26 21:06:28 +0700

    checkpoint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import time
-import random
-import math
 
 # Read File
 filename = input("Enter filename (.txt only): ")
@@ -37,15 +35,12 @@ def initCryp(arr):
     for i in arr:
         if (not(i[0] in arrFirst)): arrFirst.append(i[0])
         for j in i:
-           #if (not([j, 0] in res) and not(j.isspace())):
            if (not(j in res) and not(j.isspace())):
                 res.append(j)
     
     # Init Dict
-    #d = {'assign' : res, 'firstPos' : arrFirst, 'permutate' : [], 'combi' : [],
-    #        'arr' : arr, 'time' : 0, 'iter' : 0}
     d = {'unique' : res, 'assign' : [0 for i in range(len(res))], 
-            'firstPos' : arrFirst, 'permutate' : [], 'combi' : [],
+            'firstPos' : arrFirst, 'combi' : [],
             'arr' : arr, 'time' : 0, 'iter' : 0}
 
     return d
@@ -66,9 +61,6 @@ def checker(dicti):
             temp = dicti['assign'][dicti['unique'].index(char)]
             li.append(str(temp))
         arr.append(int(''.join(li)))
-    #        temp = [str(k[1]) for k in dicti['assign'] if k[0] == char]
-    #        li.append(temp[0])
-    #    arr.append(int(''.join(li)))
     
     #print(arr)
     #print(sum(arr[:len(arr)-1]) , "   " , arr[-1:][0])
@@ -78,67 +70,25 @@ def checker(dicti):
         dicti['combi'].append(dicti['assign'])
 
 
-# Shuffle + Timer + Iteration check
-def shuffler(dicti):
-    # Random Assign + Check firstPost
-    for i in dicti['assign']:
-        if (i[0] in dicti['firstPos']): 
-            i[1] = random.randint(1,9)
-        else:
-            i[1] = random.randint(0,9)
-
-        #print(dicti['assign'])
-        check = [True if ((j != i) and (j[1] == i[1])) else False for j in dicti['assign']]
-        #print(check)
-
-        while (True in check):
-            if (i[0] in dicti['firstPos']): 
-                i[1] = random.randint(1,9)
-            else:
-                i[1] = random.randint(0,9)
-            check = [True if ((j != i) and (j[1] == i[1])) else False for j in dicti['assign']]
-
-    #print(dicti['assign'])
 
 # Permutate
-#def permutate(dicti):
-#    dicti['iter'] = math.factorial(len(d['assign']))
-#    for i in range(dicti['iter']):   
-#        shuffler(d)
-#        dicti['permutate'].append(dicti['assign'])
-#        checker(dicti)
-    
-#    dicti['permutate'] = set(dicti['permutate'])
-
 def permutate(dicti):
-    def rand(dicti):
-        res = []
-        for i in range(len(dicti['unique'])):
-            if (dicti['unique'][i] in dicti['firstPos']):
-                temp = random.randint(1,9)
-                while (temp in res):
-                    temp = random.randint(1,9)
-            else:
-                temp = random.randint(0,9)
-                while (temp in res):
-                    temp = random.randint(0,9)
-            res.append(temp)
-        #print (res)
-        if res not in dicti['permutate']:
-            dicti['permutate'].append(res)
-        else:
-            while (res in dicti['permutate']):
-                res = rand(dicti)
-            dicti['permutate'].append(res)
+    def rand(dicti, range, length):
+        possibility = list(map(tuple, range)) * length
+        res = [[]]
+        for pos in possibility:
+            res = [x+[y] for x in res for y in pos]
+       
+        dicti['iter'] = len(res)
+        for prod in res:
+            yield tuple(prod)
 
-        return res
-
-    dicti['iter'] = int(math.factorial(10)/ math.factorial(10-len(dicti['unique']))) 
-    # This is too much, my pc can't handle
-    print("Iters: ", dicti['iter'])
-    for i in range(dicti['iter']):
-        print(i)
-        temp = rand(dicti)
+    
+    digits = ('1','2','3','4','5','6','7','8','9')
+    for x in rand(dicti, range(10), len(dicti['unique'])):
+        if len(set(x)) == len(dicti['unique']):
+            yield tuple(digits[i] for i in x)
+    
     
 
 
@@ -146,11 +96,6 @@ def permutate(dicti):
 # Timer Off
 d['time'] = time.time() - start
 
-permutate(d)
-print("HUYE SELESAI")
-for permute in d['permutate']:
-    d['assign'] = permute
-    checker(d)
 
 print(d)
 
